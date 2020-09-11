63e18f3f57cf26c9a4d0e350b2e3024b432fbfac
Ismail Essabbagh <45835323+ismailoviic@users.noreply.github.com>
2020-09-11 17:01:58 +0100

    Updates

---
+++ precache-manifest.f2dbc20c9f8e92cc2284027e91db6559.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99ca72959b4b999cbc4dbe0cc5bdbc4",
+    "url": "/sunsetOnFire-PlayTimer/index.html"
+  },
+  {
+    "revision": "56a5266cd97e318e37c9",
+    "url": "/sunsetOnFire-PlayTimer/static/css/main.8f94a867.chunk.css"
+  },
+  {
+    "revision": "b63a77d47201088a0248",
+    "url": "/sunsetOnFire-PlayTimer/static/js/2.b5433d30.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/sunsetOnFire-PlayTimer/static/js/2.b5433d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a5266cd97e318e37c9",
+    "url": "/sunsetOnFire-PlayTimer/static/js/main.9d3d7f11.chunk.js"
+  },
+  {
+    "revision": "e057aa78367194c74de6",
+    "url": "/sunsetOnFire-PlayTimer/static/js/runtime-main.d499a7c1.js"
+  },
+  {
+    "revision": "4932085e2f95cd89798b674a2764d5d0",
+    "url": "/sunsetOnFire-PlayTimer/static/media/pslogo.4932085e.png"
+  },
+  {
+    "revision": "041513fdb973ae04cb97acf4c9fa425e",
+    "url": "/sunsetOnFire-PlayTimer/static/media/soflogo.041513fd.png"
+  }
+]);
--- src/components/Logo/styles.js
+++
@@ -1,8 +0,0 @@
-import { createUseStyles } from 'react-jss'
-
-export const useStyles = createUseStyles({
-  logo: {
-    height: '300px',
-    width: '300px'
-  }
-})
